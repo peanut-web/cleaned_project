e60fda8fbd815026e85be0ae1d0c5b0f213e8a82
Vikas Reddy <reddyvikas73@gmail.com>
2025-07-12 00:08:20 +0530

    main.py is commited

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ def fetch_actions():
         doc["_id"] = str(doc["_id"])
     return jsonify(data)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
